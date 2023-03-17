bae0cba3b74ab0ec11b0830ea7c15fa49c699527
lisai <1203350716@qq.com>
2023-03-17 20:30:59 +0800

    msg:3.17晚提交

--- src/main/java/com/common/mapper/DrugMapper.java
+++ src/main/java/com/common/mapper/DrugMapper.java
@@ -1,5 +1,6 @@
 package com.common.mapper;
 
+import com.common.bean.RestBean;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +10,6 @@ import java.util.Map;
 @Mapper
 public interface DrugMapper {
     List<Map<String,Object>> getDrug();
+
+    List<Map<String,Object>> searchDrug(String drug_name, String drug_code);
 }
